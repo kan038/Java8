cd55f8bdefd70d98af5258dfce20242d0e0572f3
Kan038 <test@aaa.com>
2015-12-13 22:17:39 +0900

    first commit

---
+++ src/exsample/SDTest.java
@@ -0,0 +1,46 @@
+package exsample;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class SDTest {
+
+	public static void main(String args[]) throws Exception{
+		
+		//objectToDisk();
+		diskToObject();
+	}
+	
+	// デシリアライズ
+	private static void diskToObject() throws Exception {
+		FileInputStream inFile = new FileInputStream("C:\\tmp\\20151213\\tempdata.ser"); 
+		ObjectInputStream inObject = new ObjectInputStream(inFile);
+		
+		Object obj = inObject.readObject();
+		
+		if (obj instanceof Todo) {
+			Todo todo = (Todo)obj;
+			System.out.println("デシリアライズ + id = " + todo.getId());
+		} else {
+			System.out.println("予期しないデータ");
+		}
+
+	}
+
+	// シリアライズ
+	private static void objectToDisk() throws Exception {
+
+		Todo todo = new Todo();
+		todo.setId("777");
+        FileOutputStream fos = new FileOutputStream("C:\\tmp\\20151213\\tempdata.ser");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(todo);
+        oos.close();
+	}
+	
+	
+}
---
+++ src/exsample/Todo.java
@@ -0,0 +1,33 @@
+package exsample;
+
+import java.io.Serializable;
+
+public class Todo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private String id;
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + "]";
+	}
+	
+	
+	
+}
